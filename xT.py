315c1d6dd0267e3a9a6d99fe91c79ce807b6704f
Khaled Abuobaid <rq20135.bristol.ac.uk>
2022-03-30 15:40:05 +0300

    fixed error

--- xT.py
+++ xT.py
@@ -9,7 +9,7 @@ with open("xTdata.json") as data:
 def xT(coordinate):
     if coordinate[0] == 120:
         coordinate[0] = 110
-    if coordinate[1] == 110:
+    if coordinate[1] == 80:
         coordinate[1] = 70
     return xTval[math.floor(coordinate[1]/10),
                  math.floor(coordinate[0]/10)]
